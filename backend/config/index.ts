3126a4663d242e04c9cf7e1c87109855020950b8
Rodrigo Cid <rodrigocid95@gmail.com>
2023-02-21 02:44:06 +0000

    Crypto switch

--- backend/config/index.ts
+++ backend/config/index.ts
@@ -1,12 +1,14 @@
 import { CipherClass } from 'types/Cipher'
 import path from 'path'
 import fs from 'fs'
+import { Flags } from 'bitis/core'
 import { BitisHTTPConfigProfile } from 'bitis/http'
 import { BitisSocketsConfig, Socket } from 'bitis/web-sockets'
 import session from 'express-session'
 import fileUpload from 'express-fileupload'
 import { v4 } from 'uuid'
 
+const flags = new Flags()
 const baseDir = path.resolve(__dirname, '..', '..')
 if (!fs.existsSync(baseDir)) {
   fs.mkdirSync(baseDir, { recursive: true })
@@ -40,16 +42,22 @@ export const bitisSocketsConfig: BitisSocketsConfig = {
       return io
     },
     async onANewRequest(request, socket, getLibraryInstance) {
-      if (request[0] && typeof request[0] === 'object' && request[0].request) {
-        const cipher = getLibraryInstance<CipherClass>('cipher')
-        const data = await cipher.decrypt(socket.id, request[0].request)
-        request[0] = data
+      if (!flags.get('no-crypto')) {
+        if (request[0] && typeof request[0] === 'object' && request[0].request) {
+          const cipher = getLibraryInstance<CipherClass>('cipher')
+          const data = await cipher.decrypt(socket.id, request[0].request)
+          request[0] = data
+        }
+        return request
       }
       return request
     },
     async onBeforeToAnswer(response, socket, getLibraryInstance) {
-      const cipher = getLibraryInstance<CipherClass>('cipher')
-      return await cipher.encrypt(socket.id, JSON.stringify(response))
+      if (!flags.get('no-crypto')) {
+        const cipher = getLibraryInstance<CipherClass>('cipher')
+        return await cipher.encrypt(socket.id, JSON.stringify(response))
+      }
+      return response
     }
   }
 }
