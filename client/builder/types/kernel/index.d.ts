9c35273ff8f95f44f94dae8c8ce988095ad0e313
Rodrigo Cid <rodrigocid95@gmail.com>
2023-05-21 09:03:00 +0000

    Kernel types

--- client/builder/types/kernel/index.d.ts
+++ client/builder/types/kernel/index.d.ts
@@ -1,17 +1,29 @@
 import { IEmmiters } from "./drivers/emmiter"
 
+export interface ITask<T> {
+  PID: string
+  icon?: string
+  title: string
+  description: string
+  readonly el: T
+  kill(): void
+}
+export interface ITaskClass<T> {
+  new(el: T): ITask<T>
+}
 export interface ITaskManager {
+  run<T>(el: T): ITask<T>
 }
 export interface ITaskManagerClass {
   new(): ITaskManager
 }
-export interface IDriverManager {
-  getDriver<K extends keyof IDriverList>(name: K): Promise<IDriverList[K]>
-}
 export interface IDriverList {
   emmiters: IEmmiters
 }
-export interface IControllersManagerClass {
+export interface IDriverManager {
+  getDriver<K extends keyof IDriverList>(name: K): Promise<IDriverList[K]>
+}
+export interface IDriverManagerClass {
   new(): IDriverManager
 }
 export interface IDriver<T> {
