f87f36a015dcd1090e13421252b9cbb301f527af
Rodrigo Cid <rodrigocid95@gmail.com>
2024-04-03 00:30:17 -0600

    Fix dev mode redirection

--- backend/controllers/middlewares/dev-mode.ts
+++ backend/controllers/middlewares/dev-mode.ts
@@ -1,7 +1,7 @@
 export function devMode(_: PXIOHTTP.Request, res: PXIOHTTP.Response, next: PXIOHTTP.Next) {
   const _this: any = this
   if ((_this?.devModeModel as Models<'DevModeModel'>)?.isDevMode?.isDevMode) {
-    res.render('dev-mode', { title: 'Modo desarrollo habilitado.', description: 'La interfaz está deshabilitada.' })
+    res.redirect((_this.devModeModel as Models<'DevModeModel'>).isDevMode.cors)
   } else {
     next()
   }
