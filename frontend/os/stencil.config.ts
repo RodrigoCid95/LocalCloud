eeaca11415e541c7622cfbe559f3593f4a906fb1
Rodrigo Cid <rodrigocid95@gmail.com>
2024-05-01 02:49:14 +0000

    Base service worker

--- frontend/os/stencil.config.ts
+++ frontend/os/stencil.config.ts
@@ -2,14 +2,17 @@ import { Config } from '@stencil/core';
 
 // https://stenciljs.com/docs/config
 
-export const config: Config = {
+export const config: Config & any = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
   outputTargets: [
     {
       type: 'www',
-      serviceWorker: false,
+      serviceWorker: {
+        swDest: 'app',
+        sourcemap: false
+      },
       dir: '../../backend/public',
       buildDir: 'js'
     },
