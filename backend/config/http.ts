338b7a42731eb3cc39c697e8989e2e6f5da49087
Rodrigo Cid <rodrigocid@gmail.com>
2024-08-07 02:24:50 +0000

    Add Redit to sessions

--- backend/config/http.ts
+++ backend/config/http.ts
@@ -1,25 +1,30 @@
 import crypto from 'node:crypto'
-import session from 'express-session'
-import RedisStore from 'connect-redis'
-import { createClient } from 'redis'
 import compression from 'compression'
 import { Liquid } from 'liquidjs'
 import cors from 'cors'
 import { paths } from './paths'
+import RedisStore from "connect-redis"
+import session from "express-session"
+import { createClient } from "redis"
 
 declare const isRelease: boolean
 declare const flags: PXIO.Flags
 
 const redisClient = createClient()
+redisClient.connect().catch(console.error)
+
+const redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "lc:",
+})
 
 const middlewares = [
   compression(),
   session({
-    store: new RedisStore({ client: redisClient }),
-    secret: crypto.randomUUID(),
+    store: redisStore,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false }
+    secret: crypto.randomUUID(),
   })
 ]
 
@@ -40,6 +45,9 @@ export const HTTP: PXIOHTTP.Config = {
   },
   middlewares,
   events: {
+    afterConfig(app) {
+      app.set('trust proxy', 1)
+    },
     onError(err, _, res, next) {
       if (err) {
         res.status(500).json(err)
