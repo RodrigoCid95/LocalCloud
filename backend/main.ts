13e23b47d22942a418a5d6cd870c586fe52d1d1d
Rodrigo Cid <rodrigocid@gmail.com>
2024-09-06 15:54:05 -0600

    Quit socket server

--- backend/main.ts
+++ backend/main.ts
@@ -1,12 +1,9 @@
-import { createServer } from 'node:net'
-import fs from 'node:fs'
-import path from 'node:path'
 import cluster from "node:cluster"
 
-const initSessionServer = (SOCKET_PATH: string) => new Promise<void>(resolve => {
-  if (fs.existsSync(SOCKET_PATH)) {
-    fs.rmSync(SOCKET_PATH, { force: true })
-  }
+if (cluster.isPrimary) {
+  const os = require('node:os')
+  const numCPUs = os.availableParallelism()
+  console.log(`\n\nMaster ${process.pid} is running`, `\n${numCPUs} workers:\n`)
   const Store = {
     store: new Map(),
     get: (sid: string) => Store.store.get(sid) || null,
@@ -25,37 +22,16 @@ const initSessionServer = (SOCKET_PATH: string) => new Promise<void>(resolve => 
     all: () => Array.from(Store.store.values()),
     clear: () => Store.store.clear()
   }
-  const sessionServer = createServer(socket => {
-    socket.on('data', (data) => {
-      const strData = data.toString()
-      const parts = strData
-        .split('\n')
-        .filter(part => part !== '')
-        .map(part => JSON.parse(part))
-      for (const part of parts) {
-        const { uid, event, args = [] } = part
-        const e = Store[event]
-        const result = e(...args)
-        const message = JSON.stringify({ uid, data: result })
-        socket.write(`${message}\n`)
-      }
+  const PORTS = Array.from({ length: numCPUs }, (_, i) => 3000 + i)
+  for (const PORT of PORTS) {
+    const child = cluster.fork({ PORT })
+    child.on('message', message => {
+      const { uid, event, args = [] } = message
+      const e = Store[event]
+      const result = e(...args)
+      child.send({ uid, data: result })
     })
-  })
-  sessionServer.listen(SOCKET_PATH, resolve)
-})
-
-if (cluster.isPrimary) {
-  const sessionSockPath = path.resolve(process.cwd(), 'session-server.sock')
-  initSessionServer(sessionSockPath)
-    .then(() => {
-      const os = require('node:os')
-      const numCPUs = os.availableParallelism()
-      console.log(`\n\nMaster ${process.pid} is running`, `\n${numCPUs} workers:\n`)
-      const PORTS = Array.from({ length: numCPUs }, (_, i) => 3000 + i)
-      for (const PORT of PORTS) {
-        cluster.fork({ PORT })
-      }
-    })
+  }
 } else {
   initHttpServer({ onMessage: console.log })
 }
